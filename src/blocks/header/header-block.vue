2c6947b656a8659f665704ab16a6ccc4d81c0e80
sathelo <sashenkocasha@mail.ru>
2023-06-07 18:44:26 +0300

    feat: Добавил шапку, карточку

--- src/blocks/header/header-block.vue
+++ src/blocks/header/header-block.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="header">Header block</div>
+  <div class="header">
+    <div class="header-logos">
+      <div class="header-logos-logo">
+        <img
+          class="header-logos-logo__kotex"
+          :src="getStaticUrl('logo-kotex.svg')"
+          alt="logo-kotex"
+        />
+      </div>
+      <div class="header-logos-logo">
+        <img
+          class="header-logos-logo__perehod"
+          :src="getStaticUrl('logo-perehod.svg')"
+          alt="logo-perehod"
+        />
+      </div>
+    </div>
+
+    <div class="header-nav">
+      <a v-for="(section, id) in menu" :key="id" :href="section.item.link" class="header-nav-item">
+        {{ section.item.title }}
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'header-block',
+  computed: {
+    menu() {
+      return this.$store.state.bunker.menu;
+    },
+  },
 };
 </script>
 
--- src/blocks/main/main-block.vue
+++ src/blocks/main/main-block.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="main">Main block</div>
+  <div class="main">
+    <div class="main-block">
+      <div class="main-block__content">
+        <div class="main-block-info">
+          <div class="main-block-info__title">
+            &laquo;Твой первый раз&raquo; уникальный образовательный курс для родителей
+            и&nbsp;подростков от&nbsp;бренда Kotex<sup class="reg">&reg;</sup> совместно
+            с&nbsp;проектом Переход
+          </div>
+
+          <div class="main-block-info__subtitle">
+            Расскажем всё об&nbsp;этапах и&nbsp;особенностях переходного возраста, поможем построить
+            доверительные взаимоотношения и&nbsp;укрепить&nbsp;их, а&nbsp;также разобраться, как
+            меняется поведение и&nbsp;организм в&nbsp;пубертатный период.
+          </div>
+        </div>
+
+        <img :src="getStaticUrl('photo-block-1.png')" alt="photo" class="main-block__photo" />
+      </div>
+    </div>
+
+    <div class="main-block-2">
+      <div class="main-block-2__content">
+        <div class="main-block-2-info">
+          <img
+            :src="getStaticUrl('logo-perehod.svg')"
+            alt="logo-perehod"
+            class="main-block__logo"
+          />
+
+          <div class="main-block-2-info__title">
+            Проект наставничества, который помогает Подросткам счастливо прожить переходный возраст,
+            сформировать взрослую модель поведения и&nbsp;сохранить доверительные отношения
+            с&nbsp;Родителями.
+          </div>
+
+          <div class="main-block-2-info__btn">
+            Расскажем всё об&nbsp;этапах и&nbsp;особенностях переходного возраста, поможем построить
+            доверительные взаимоотношения и&nbsp;укрепить&nbsp;их, а&nbsp;также разобраться, как
+            меняется поведение и&nbsp;организм в&nbsp;пубертатный период.
+          </div>
+        </div>
+
+        <img :src="getStaticUrl('photo-block-1.png')" alt="photo" class="main-block__photo" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
