66400df6b229b007a22c8599ad500c6e0f725e3a
sathelo <sathelo@yandex.ru>
2023-06-18 23:43:03 +0300

    feat: Блок событий

--- src/blocks/afisha-event/afisha-event-block.vue
+++ src/blocks/afisha-event/afisha-event-block.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <div class="afisha-event">{{ afishaEvent() }}</div>
+  <div class="afisha-event">
+    <div class="afisha-event-heart">
+      <img
+        :src="getStaticUrl('heart-fill.svg')"
+        alt="heart-fill"
+        class="afisha-event-heart__fill"
+      />
+      <img
+        :src="getStaticUrl('heart-empty.svg')"
+        alt="heart-empty"
+        class="afisha-event-heart__empty"
+      />
+    </div>
+
+    <div class="afisha-event-info">
+      <div class="afisha-event-info__title">Афиша событий</div>
+      <div class="afisha-event-info__subtitle">
+        Подборка событий для совместного посещения родителями и&nbsp;подростками
+      </div>
+      <div class="afisha-event-info-select">
+        <div class="afisha-event-info-select__text">Москва</div>
+        <img
+          :src="getStaticUrl('arrow-bottom-ico.svg')"
+          alt="arrow-bottom"
+          class="afisha-event-info-select__ico"
+        />
+      </div>
+    </div>
+
+    <afisha-event-calendar-block />
   </div>
 </template>
 
---
+++ src/blocks/afisha-event/afisha-event-calendar-block.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="calendar">
+    <div class="calendar-events">
+      <div
+        v-for="(event, eventId) in events"
+        :key="eventId"
+        class="calendar-events-event"
+        :class="{ 'calendar-events-event--active': event.event.isActive }"
+      >
+        {{ event.event.title }}
+      </div>
+    </div>
+
+    <div class="calendar-months">
+      <div class="calendar-months-month">123</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'afisha-event-calendar-block',
+  computed: {
+    events() {
+      return this.$store.state.bunker.calendar.events;
+    },
+    months() {
+      return this.$store.state.bunker.calendar.months;
+    },
+  },
+};
+</script>
+
+<style src="./afisha-event-calendar-block.less" lang="less"></style>
